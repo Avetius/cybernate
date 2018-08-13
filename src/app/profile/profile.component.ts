2fbeae6d649d3ab2f3ccc136bc33a6f4a48dbef6
dataowl <dataowl@dataowls-MacBook-Pro-2.local>
2018-08-13 23:44:37 +0400

    mac

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -2,28 +2,31 @@ import { Component, OnInit } from '@angular/core';
 import { first } from 'rxjs/operators';
 import { UserModel } from '../_models';
 import { UserService } from '../_services';
-import { FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     templateUrl: 'profile.component.html',
 })
+
 export class ProfileComponent implements OnInit {
-    public users: UserModel[];
-    loginForm: FormGroup;
-    loading = false;
-    submitted = false;
-    returnUrl: string;
-    error = '';
+    public profileForm: FormGroup;
+    public users: UserModel[] = [];
+    public loading = false;
+    public submitted = false;
+    public returnUrl: string;
+    public error = '';
 
     constructor(
         private userService: UserService,
-        private formBuilder: FormBuilder,
-        private route: ActivatedRoute,
-        private router: Router,
-        private authenticationService: AuthenticationService) {}
+        private formBuilder: FormBuilder
     ) {}
 
     ngOnInit() {
+        this.profileForm = this.formBuilder.group({
+            username: ['', Validators.required],
+            password: ['', Validators.required]
+        });
+
         this.userService.getAll().pipe(first()).subscribe(users => {
             this.users = users;
         });
